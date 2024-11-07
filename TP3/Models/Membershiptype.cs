86d41863ae7c05805226c78c26f829a5e6eed1b0
Yassine Sallemi <yassine.sallemi.251@gmail.com>
2024-11-07 22:52:21 +0100

    feat: tp3 init commit

---
+++ TP3/Models/Membershiptype.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TP3.Models;
+
+public partial class Membershiptype
+{
+    public int Id { get; set; }
+
+    public decimal? SignUpFee { get; set; }
+
+    public int? DurationInMonth { get; set; }
+
+    public decimal? DiscountRate { get; set; }
+
+    public virtual ICollection<Customer> Customers { get; set; } = new List<Customer>();
+}
